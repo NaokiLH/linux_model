72d2ea9e01985cde93a3de29a9a95ddfa20d5d8d
naokilh <821874555@qq.com>
2021-11-21 13:23:55 +0800

    add tests

--- test_for_linux_module/tests/test_pipe_dev.rs
+++ test_for_linux_module/tests/test_pipe_dev.rs
@@ -7,32 +7,63 @@ use std::{
 #[test]
 fn test_multi_write_and_read() {
     let num = rand::thread_rng().gen_range(50..=100);
-    for _ in 0..num {
-        test_multi_write();
-    }
-}
-fn test_multi_write() {
-    let pipe_dev = OpenOptions::new()
-        .write(true)
-        .read(true)
-        .open("/dev/pipe_dev")
-        .unwrap();
     let mut handle_list = Vec::new();
-    let thread_num = rand::thread_rng().gen_range(10..20);
-    println!("thread_num: {}", thread_num);
-    for _ in 0..thread_num {
-        let buf = "12345";
-        let mut pipe = pipe_dev.try_clone().unwrap();
+    for _ in 0..num {
         let handle = thread::spawn(move || {
-            pipe.write(buf.as_bytes()).unwrap();
-            pipe.flush().unwrap();
+            let mut pipe_dev = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .open("/dev/pipe_dev")
+                .unwrap();
+
+            pipe_dev.write(b"12345").unwrap();
         });
         handle_list.push(handle);
     }
+
     for handle in handle_list {
         handle.join().unwrap();
     }
-    check_file_content(thread_num);
+
+    check_file_content(num);
+}
+#[test]
+fn test_multi_empty_read() {
+    let num = rand::thread_rng().gen_range(50..=100);
+
+    for _ in 0..num {
+        thread::spawn(|| {
+            let mut pipe_dev = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .open("/dev/pipe_dev")
+                .unwrap();
+
+            let mut buf = Vec::new();
+
+            pipe_dev.read_to_end(&mut buf).unwrap();
+
+            assert_eq!(buf.len(), 0);
+        });
+    }
+}
+#[test]
+fn test_multi_empty_write() {
+    let num = rand::thread_rng().gen_range(50..=100);
+
+    for _ in 0..num {
+        thread::spawn(|| {
+            let mut pipe_dev = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .open("/dev/pipe_dev")
+                .unwrap();
+
+            let mut buf = Vec::new();
+
+            pipe_dev.write_all(&mut buf).unwrap();
+        });
+    }
 }
 
 fn check_file_content(thread_num: usize) {
